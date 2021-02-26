ec1450c47b83f2feb5a12ac6b6d10eaf52622195
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-26 08:55:41 -0800

    Test 2 good pass and a test 1 rewrite

--- lib/main.rb
+++ lib/main.rb
@@ -3,8 +3,11 @@ class Word_Check
     def anagram (word_one, word_two)
       first_word = word_one.split('').sort
       second_word = word_two.split('').sort
-      return [first_word, second_word]
+      if (first_word == second_word)
+        'These are anagrams.'
+      else
+        'These are not anagrams'
+      end
 
     end
-
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -5,7 +5,11 @@ describe(Word_Check) do
   describe('#anagram') do
     it ('will take 2 arguments convert them to an array with 2 arrays and sort them alphabetically') do
     words = Word_Check.new()
-    expect(words.anagram('time', 'mite')).to(eq([['e','i','m','t'],['e','i','m','t']]))
+    expect(words.anagram('time', 'mite')).to(eq('These are anagrams.'))
+    end
+    it ('will take 2 arguments see if they are anagrams') do
+    words = Word_Check.new()
+    expect(words.anagram('time', 'mite')).to(eq('These are anagrams.'))
     end
   end
 end
