90bb3619f6891a302c47dd6c27b881b2b88c852f
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-26 08:39:14 -0800

    Test 1 good pass

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,10 @@
 class Word_Check
-  def
 
-    def anagram
+    def anagram (word_one, word_two)
+      first_word = word_one.split('').sort
+      second_word = word_two.split('').sort
+      return [first_word, second_word]
+
     end
 
-  end
+end
